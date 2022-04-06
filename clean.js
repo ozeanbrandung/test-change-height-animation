b885e2c187b6321944ad84249a911d68b8cfd0a1
Natalya Molchanova <natalia.v.molchanova@yandex.ru>
2022-04-06 19:55:48 +0400

    make other block change height

--- clean.js
+++ clean.js
@@ -37,16 +37,16 @@ class ClassWatcher {
 //после полной прогрузки страницы:
 document.addEventListener("DOMContentLoaded", function(){
     const contents = document.querySelectorAll("#vkladki .lc-tabs__content");
-    const vkladki = document.getElementById("vkladki");
+    const blockWithSmoothingHeight = document.querySelector("#vkladki .lc-tabs");
     const initialActiveItem = document.querySelector("#vkladki .lc-tabs__content.lc-tabs__content_visible");
     const tabsHeight = document.querySelector("#vkladki .lc-tabs__scroll-wrapper").offsetHeight;
 
     /* первая инициализиция атрибута style　для общего контейнера */
-    vkladki.style.height = initialActiveItem.offsetHeight + tabsHeight + 'px';
+    blockWithSmoothingHeight.style.height = initialActiveItem.offsetHeight + tabsHeight + 'px';
 
     /* при добавлении класса меняем высоту родителя в зависимости от высоты отображаемого сейчас нода */
     const workOnClassAdd = (node) => {
-        vkladki.style.height = tabsHeight + node.scrollHeight + 'px';
+        blockWithSmoothingHeight.style.height = tabsHeight + node.scrollHeight + 'px';
     }
 
     //устанавливаем слежку за каждым элементом контента
